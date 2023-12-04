bdc24f33217226a08c0d4274f71832ea3944e017
Plaifah <praypar5689@gmail.com>
2023-12-04 15:25:12 +0700

    first commit

---
+++ bin/moderm_week8.dart
@@ -0,0 +1,5 @@
+import 'package:moderm_week8/moderm_week8.dart' as moderm_week8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${moderm_week8.calculate()}!');
+}
---
+++ test/moderm_week8_test.dart
@@ -0,0 +1,8 @@
+import 'package:moderm_week8/moderm_week8.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
